b715c70b1f96b1076608c027eba4ad3992cd421a
GuillermoTorrez <guillermo.torrez@gmail.com>
2021-05-30 20:26:29 -0400

    Update Setting MailHost

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -173,13 +173,13 @@ if not DEBUG:
     SECURE_HSTS_PRELOAD = True
     X_FRAME_OPTIONS = "DENY"
     ALLOWED_HOSTS = ["*"]
-    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-    EMAIL_HOST = env("EMAIL_HOST")
-    EMAIL_HOST_USER = env("EMAIL_HOST_USER")
-    EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
-    EMAIL_USE_TLS = True
-    EMAIL_PORT = env("EMAIL_PORT")
-    DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL")
+    #EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+    #EMAIL_HOST = env("EMAIL_HOST")
+    #EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+    #EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+    #EMAIL_USE_TLS = True
+    #EMAIL_PORT = env("EMAIL_PORT")
+    #DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL")
 
 LOGGING = {
     'version': 1,
